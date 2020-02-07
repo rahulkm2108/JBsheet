467f53f82e616c566c26bf8355c48acfc3224b6e
rahulkumar.mishra <rahulkm39@gmail.com>
2020-02-07 13:03:23 +0530

    Updated UI

---
+++ src/app/dialog-box/dialog-box.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { VERSION, MatDialogRef, MatDialog, MatSnackBar, MAT_DIALOG_DATA } from '@angular/material';
+
+@Component({
+  selector: 'app-dialog-box',
+  templateUrl: './dialog-box.component.html',
+  styleUrls: ['./dialog-box.component.css']
+})
+export class DialogBoxComponent implements OnInit {
+
+  message: string = ""
+  cancelButtonText = "Cancel"
+  constructor(
+    @Inject(MAT_DIALOG_DATA) private data: any,
+    private dialogRef: MatDialogRef<DialogBoxComponent>) {
+    if (data) {
+      this.message = data.message || this.message;
+      if (data.buttonText) {
+        this.cancelButtonText = data.buttonText.cancel || this.cancelButtonText;
+      }
+    }
+    this.dialogRef.updateSize('300vw','300vw')
+  }
+
+  onConfirmClick(): void {
+    this.dialogRef.close(true);
+  }
+
+
+  ngOnInit() {
+  }
+
+}
